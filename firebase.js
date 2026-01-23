897af96469fb3d8fe717a7b5902b79fdbcf48cb3
Hwang-John <113040867+Hwang-John@users.noreply.github.com>
2026-01-23 10:10:33 +0900

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDu9yow7A2AVPB-nNILrY33dW9pvBzqg5U",
+    authDomain: "tech-entrepreneur-invest.firebaseapp.com",
+    projectId: "tech-entrepreneur-invest",
+    storageBucket: "tech-entrepreneur-invest.firebasestorage.app",
+    messagingSenderId: "992495497400",
+    appId: "1:992495497400:web:24241b69d04bbbd684444d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
